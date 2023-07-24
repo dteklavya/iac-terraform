4d659a7160cd92886da6cb41311d0e6d4818c153
Rajesh P <rajesh.pethe@gmail.com>
2023-07-24 11:34:03 +0530

    Don't need the EC2 instance yet.

--- main.tf
+++ main.tf
@@ -8,14 +8,14 @@ provider "aws" {
     region = var.aws_region
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
+# resource "aws_instance" "web" {
+#   ami           = "ami-0f5ee92e2d63afc18"
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "first-tf-ec2-instance"
-  }
-}
+#   tags = {
+#     Name = "first-tf-ec2-instance"
+#   }
+# }
 
 resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
