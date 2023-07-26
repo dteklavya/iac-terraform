83ba4233497639e646e727ddd92cc2673a92ca3b
Rajesh P <rajesh.pethe@gmail.com>
2023-07-26 11:54:24 +0530

    Create a network interface with an private ip in the subnet.

--- main.tf
+++ main.tf
@@ -115,3 +115,15 @@ resource "aws_security_group" "allow_web" {
     Name = "allow_web_ssh"
   }
 }
+
+# Create a network interface with an ip in the subnet
+resource "aws_network_interface" "web-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+  # attachment {
+  #   instance     = "${aws_instance.test.id}"
+  #   device_index = 1
+  # }
+}
